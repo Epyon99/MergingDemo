f514157fdb55746965cfcd78d0f66ca6049374b3
Moises Chirinos <moises.chirinos@leuchterag.ch>
2019-02-15 11:33:47 -0500

    @proyecto: primer commit

---
+++ Proyecto/ConsoleSuperApp/ConsoleSuperApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleSuperApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void Despedir(string frase)
+        {
+            
+        }
+
+        static void Saludos(string frase)
+        {
+
+        }
+
+        static void Comer (string frase)
+        {
+
+        }
+    }
+}
